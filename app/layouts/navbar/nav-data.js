377a8692c454793345d12a1023e36c9807e86196
honokanishimura <honokanishimura1008@gmail.com>
2025-05-27 16:43:26 +0900

    Fix: update all files after local changes

--- app/layouts/navbar/nav-data.js
+++ app/layouts/navbar/nav-data.js
@@ -13,7 +13,7 @@ export const navLinks = [
   },
   {
     label: 'Resume',
-    pathname: 'https://docs.google.com/document/d/1yXbXzaLTlC7Cb3q3M70yiiKiJFy7jZAeGnYA_26d_ag/edit?usp=sharing',
+    pathname: '/resume.pdf',
   },
   
   {
